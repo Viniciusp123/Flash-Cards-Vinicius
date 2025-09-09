02b01ce04f6ea88902514ad2543b050292eacb87
Viniciusp123 <vinicius.paulichen@escola.pr.gov.br>
2025-09-09 08:46:10 -0300

    09

--- pergutas.js
+++ pergutas.js
@@ -1,1 +1,50 @@
-criaCARTAO()
+criaCartao(
+    'Conhecimento',
+    'Quanto tempo a luz do Sol demora para chegar à Terra?',
+    '8 minutos'
+)
+criaCartao(
+    'Conhecimento',
+    'Em que ano o homem pisou na lua?',
+    '1969'
+)
+criaCartao(
+    'Conhecimento',
+    'Qual ave é capaz de bater as asas até 200 vezes por segundo?',
+    'O beija-flor'
+)
+criaCartao(
+    'Conhecimento',
+    'Quantos ossos temos no nosso corpo?',
+    '206'
+)
+criaCartao(
+    'Geografia',
+    'Qual é o menor país do mundo?',
+    'Vaticano'
+)
+criaCartao(
+    'conhecimento gerais',
+    'Por qual apelido ficou conhecido o jogador de futebol Edson Arantes?',
+    'Pelé'
+)
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
