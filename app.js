c74e8aa0a61befaa267ab0df3574177d06c65463
Viniciusp123 <vinicius.paulichen@escola.pr.gov.br>
2025-09-16 10:33:38 -0300

    16

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function criaCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article');
     cartao.className = 'cartao'
 
-    cartao.inenerHTML=`
+    cartao.innerHTML=`
     < div class="cartao__conteudo" >
     <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
